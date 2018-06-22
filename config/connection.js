12363b858423d1523f7774fb91f6574db44ca091
Mike West <mister270@gmail.com>
2018-06-21 23:05:20 -0700

    correct JAWSDB connection

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var keys = require("../config/keys.js")
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env, JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   var connection = mysql.createConnection({
     host: "localhost",
